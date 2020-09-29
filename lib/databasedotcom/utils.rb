9d13d82aa427affb94fdb020a3d60aaf57656c15
Miguel Botelho <jmbotelho95@gmail.com>
2020-09-29 11:30:39 +0100

    Add rescue clause on JSON parse

---
+++ lib/databasedotcom/utils.rb
@@ -0,0 +1,19 @@
+module Databasedotcom
+  module Utils
+    def self.emoji_safe_json_parse(data)
+      JSON.parse(data)
+    rescue JSON::ParserError => e
+      raise e unless e.message.include? 'incomplete surrogate pair'
+
+      matches = data.scan(/(\\u[0-9|a-f|A-F]{4})+/)
+
+      matches.each do |match|
+        next unless match.length % 12 != 0
+
+        fixed_match = match[0...-6]
+        data.gsub!(match, fixed_match)
+      end
+      JSON.parse(data)
+    end
+  end
+end
