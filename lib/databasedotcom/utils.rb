fcce8d381668989ec9958637440802d5fee3d87f
David Azevedo <davidazevedo1996@hotmail.com>
2020-10-09 10:35:38 +0100

    Improve code readability

--- lib/databasedotcom/utils.rb
+++ lib/databasedotcom/utils.rb
@@ -5,16 +5,28 @@ module Databasedotcom
     rescue JSON::ParserError => e
       raise e unless e.message.include? 'incomplete surrogate pair'
 
-      matches = data.scan(/(?:\\u[0-9|a-f|A-F]{4})+"/)
+      matches = find_unicode_sequences(data)
 
       matches.each do |match|
-        next if ((match.length - 1) % 12).zero?
+        next if valid_unicode_sequence?(match)
 
-        fixed_match = match[0...-7]
+        fixed_match = remove_incomplete_surrogate_pair(match)
         data.sub!(match, fixed_match + '"')
       end
 
       JSON.parse(data)
+    end
+
+    def find_unicode_sequences(data)
+      data.scan(/(?:\\u[0-9|a-f|A-F]{4})+"/)
+    end
+
+    def valid_unicode_sequence?(match)
+      ((match.length - 1) % 12).zero?
+    end
+
+    def remove_incomplete_surrogate_pair(match)
+      match[0...-7]
     end
   end
 end
